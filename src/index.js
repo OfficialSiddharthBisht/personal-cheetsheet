74734db5783664cb17b51229c0982d6df0fd8825
Siddharth <officialsiddharthbisht@gmail.com>
2023-01-03 16:29:26 +0530

    ADD: Layout using outlet

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = document.getElementById('root');
 ReactDOM.render(
-  <App />,
+  <BrowserRouter >
+    <App />
+  </BrowserRouter>,
   root
 );
